7164e44547947fc31f6eb5b53c217a1be57aa79f
Chandesh Mehta <mehtachandesh@gmail.com>
2024-09-16 15:18:25 +0530

    spectre config changes

--- lua/cm/plugins/spectre.lua
+++ lua/cm/plugins/spectre.lua
@@ -7,13 +7,10 @@ return {
 		-- Configure Spectre
 		spectre.setup({
 			color_devicons = true,
-			-- Ensure that Spectre opens in a floating window
 			open_cmd = "lua require('spectre').open()",
-			-- Optionally, you can specify floating settings here if supported
-			-- For custom floating configuration, please refer to Spectre documentation
 		})
 
-		-- Define a function to open Spectre in a floating window
+		-- Function to open Spectre in a floating window
 		local function open_spectre_floating()
 			local buf = vim.api.nvim_create_buf(false, true)
 			local opts = {
@@ -25,22 +22,18 @@ return {
 				border = "rounded",
 			}
 			local win = vim.api.nvim_open_win(buf, true, opts)
-
-			-- Open Spectre in the floating window
 			vim.api.nvim_win_set_buf(win, buf)
 			vim.cmd("silent! lua require('spectre').open()")
 		end
 
-		-- Key mappings for Spectre
-		keymap.set("n", "<leader>sr", function()
-			open_spectre_floating()
-		end, { desc = "Open Spectre in floating window for search and replace" })
-
+		-- Key mapping for Spectre
 		keymap.set(
 			"n",
-			"<leader>sw",
-			"<cmd>lua require('spectre').open_visual()<cr>",
-			{ desc = "Search and replace word under cursor" }
+			"<leader>sr",
+			open_spectre_floating,
+			{ desc = "Open Spectre in floating window for search and replace" }
 		)
+		-- TODO: Need to check why Spectre open_visual is not working with floating window.
+		-- TODO: Fix Spectre help options' window posisioning.
 	end,
 }
